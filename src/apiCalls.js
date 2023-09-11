0c49ee30c8d26b98ab9e2b1287931e075129697a
Judy Ye <judyy.ye@gmail.com>
2023-09-11 10:55:02 -0700

    Add post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,6 +8,21 @@ const getReservations = () => {
   })
 }
 
+const postReservation = (inputReservation) => {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST', 
+    body: JSON.stringify(inputReservation),
+    headers: {'Content-Type': 'application/json'}
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw new Error (`${res.status}: Something went wrong`)
+    }
+    return res.json()
+  })
+}
+
 export {
-  getReservations
+  getReservations,
+  postReservation
 }
