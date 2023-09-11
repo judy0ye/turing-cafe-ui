9bcfbcc9b9ff9295a80c3cd020ceff102d1ca1c0
Judy Ye <judyy.ye@gmail.com>
2023-09-11 08:56:00 -0700

    Add controlled form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,60 @@
+import { useState } from 'react'
 import './Form.css'
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    date: '',
+    time: '',
+    number: '' 
+  })
+
+  console.log({formData})
+  const handleChange = e => {
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  }
+
   return (
     <form>
-      <input />          
+      <label htmlFor='name' />
+      <input 
+        id='name'
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={formData.name}
+        onChange={handleChange}
+      />          
+      <label htmlFor='date' />
+      <input 
+        id='date'
+        type='date'
+        placeholder='Date (mm/dd)'
+        name='date'
+        value={formData.date}
+        onChange={handleChange}
+      />          
+      <label htmlFor='time' />
+      <input 
+        id='time'
+        type='time'
+        placeholder='Enter your Name here'
+        name='time'
+        value={formData.time}
+        onChange={handleChange}
+      />          
+      <label htmlFor='number' />
+      <input 
+        id='number'
+        type='number'
+        placeholder='Number of guests'
+        name='number'
+        value={formData.number}
+        onChange={handleChange}
+      />          
+      <button>Make a Reservation</button>
+    
     </form>
   )
 }
